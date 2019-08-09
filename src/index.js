e28a44d4e47ecf6a3929997a9efd7496607f589c
Полицинский Артем Сергеевич <artifishional@mail.ru>
2019-08-09 18:10:38 +0300

    - init

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const application = new PIXI.Application({
 });
 document.body.appendChild(application.view);
 
+const UPS = 50;
+
+const startttmp = window.performance.now();
+let worldCounter = 0;
+setInterval( function () {
+  const current = window.performance.now();
+  const count = (current - startttmp) * UPS / 1000 - worldCounter;
+  for (let i = 0; i < count; i ++ ) {
+    worldCounter ++ ;
+    model.update();
+  }
+}, 10 );
+
 
 const model = new ShipModel();
 const view = new ShipView(application.stage, model);
--- src/model/ship.js
+++ src/model/ship.js
@@ -2,10 +2,19 @@ export default class Ship {
 
   constructor () {
     this.x = 500;
+    this.worldCounter = 0;
   }
 
   update() {
-    this.x = 500;
+    this.worldCounter ++ ;
+
+    if(this.worldCounter % 1000 > 500) {
+      this.x += 1;
+    }
+    else {
+      this.x -= 1;
+    }
+
   }
 
 }
