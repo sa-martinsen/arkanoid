8ab5ad2ee2c3abc134bcfb901c6cf2237190a578
icwet <cai.cwet@gmail.com>
2019-08-19 11:47:40 +0300

    add ship container

--- src/model/ship.js
+++ src/model/ship.js
@@ -6,6 +6,10 @@ export default class Ship {
     this.move = {
       direction: 'none',
     };
+    this.container = {
+      max: 1655,
+      min: 25,
+    };
     document.addEventListener('keydown', e => {
       if (e.key === 'ArrowRight') {
         this.move = {
@@ -25,9 +29,9 @@ export default class Ship {
   }
 
   update() {
-    if (this.move.direction === 'left') {
+    if (this.move.direction === 'left' && this.x >= this.container.min) {
       this.x -= this.speed;
-    } else if (this.move.direction === 'right') {
+    } else if (this.move.direction === 'right' && this.x <= this.container.max) {
       this.x += this.speed;
     }
   }
