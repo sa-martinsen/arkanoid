6028c7c12dae1a772c058cdd6b9e1b4b48469908
icwet <cai.cwet@gmail.com>
2019-08-19 13:09:47 +0300

    fix ball and air classes view

--- src/view/air.js
+++ src/view/air.js
@@ -1,10 +1,12 @@
 import * as PIXI from 'pixi.js';
 import airImg from '../../res/air.svg';
+import View from './view';
 
 const Sprite = PIXI.Sprite,
   Texture = PIXI.Texture;
-export default class Air {
+export default class Air extends View {
   constructor(scene, model) {
+    super(scene, model);
     this.sprite = new Sprite(Texture.from(airImg));
     scene.addChild(this.sprite);
     this.model = model;
--- src/view/ball.js
+++ src/view/ball.js
@@ -1,11 +1,13 @@
 import * as PIXI from 'pixi.js';
 import ballImg from '../../res/ball.svg';
+import View from './view';
 
 const Sprite = PIXI.Sprite,
   Texture = PIXI.Texture;
 
-export default class Ball {
+export default class Ball extends View {
   constructor(scene, model) {
+    super(scene, model);
     this.sprite = new Sprite(Texture.from(ballImg));
     scene.addChild(this.sprite);
     this.model = model;
